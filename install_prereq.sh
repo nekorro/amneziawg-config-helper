ce688caa515db5c3c9dee72240fed8fdf6656857
nekorro <nekorro@gmail.com>
2024-11-10 13:29:55 +0300

    fix: install_prereq.sh

--- install_prereq.sh
+++ install_prereq.sh
@@ -9,7 +9,7 @@ fi
 echo "net.ipv4.ip_forward = 1" >/etc/sysctl.d/00-amnezia.conf
 # Add deb-src source
 cp /etc/apt/sources.list.d/ubuntu.sources /etc/apt/sources.list.d/ubuntu.sources.bak
-sed "s/^Types: deb$/^Types: deb-src$/" /etc/apt/sources.list.d/ubuntu.sources.bak >/etc/apt/sources.list.d/ubuntu.sources
+sed "s/^Types: deb$/Types: deb-src/" /etc/apt/sources.list.d/ubuntu.sources.bak >/etc/apt/sources.list.d/ubuntu.sources
 apt update -y && apt upgrade -y
 # Install awg
 add-apt-repository -y ppa:amnezia/ppa && apt install -y amneziawg
