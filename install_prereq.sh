8b48bfc6c124eb4cef1b35cc4e0bec9febb18517
nekorro <nekorro@gmail.com>
2024-11-10 17:02:45 +0300

    feat(install_prereq): do not upgrade

--- install_prereq.sh
+++ install_prereq.sh
@@ -11,7 +11,7 @@ echo "net.ipv4.ip_forward = 1" >/etc/sysctl.d/00-amnezia.conf
 if [ ! -f /etc/apt/sources.list.d/ubuntu-deb-src.sources ]; then
   cat /etc/apt/sources.list.d/ubuntu.sources | grep -v "^#" | sed "s/^Types: deb$/Types: deb-src/" >/etc/apt/sources.list.d/ubuntu-deb-src.sources
 fi
-apt update -y && apt upgrade -y
+apt update -y
 # Install awg
 add-apt-repository -y ppa:amnezia/ppa && apt install -y amneziawg
 apt install xxd
