39eb01ce5e989c4d65e8b18ab03f5b355f75f56e
nekorro <nekorro@gmail.com>
2024-11-23 11:28:25 +0300

    Update install_prereq.sh

--- install_prereq.sh
+++ install_prereq.sh
@@ -7,6 +7,7 @@ fi
 
 # Allow forwarding
 echo "net.ipv4.ip_forward = 1" >/etc/sysctl.d/00-amnezia.conf
+sysctl -p /etc/sysctl.d/00-amnezia.conf
 # Add deb-src source
 if [ ! -f /etc/apt/sources.list.d/ubuntu-deb-src.sources ]; then
   cat /etc/apt/sources.list.d/ubuntu.sources | grep -v "^#" | sed "s/^Types: deb$/Types: deb-src/" >/etc/apt/sources.list.d/ubuntu-deb-src.sources
